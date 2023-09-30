a8e41fa7a5850b4b32257aa1d87a2a0ff9db7477
AdrienPiombini <adrien.piombini@gmail.com>
2023-09-30 11:39:01 +0200

    refracto + ajout d'un service graph

--- src/app/pages/details-olympic/details-olympic.component.ts
+++ src/app/pages/details-olympic/details-olympic.component.ts
@@ -4,8 +4,10 @@ import { Color, ScaleType } from '@swimlane/ngx-charts';
 import { Olympic } from 'src/app/core/models/Olympic';
 import { ChartLine } from 'src/app/core/models/ChartLine';
 import { OlympicService } from 'src/app/core/services/olympic.service';
-import { OlympicsMedals } from 'src/app/core/models/OlympicsMedals';
+import { FormatedParticipation } from 'src/app/core/models/FormatedParticipation';
 import { ActivatedRoute } from '@angular/router';
+import { Participation } from 'src/app/core/models/Participation';
+import { ChartGraphService } from 'src/app/core/services/chartGraph.service';
 
 
 @Component({
@@ -15,67 +17,60 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DetailsOlympicComponent {
 
-  view: [number, number] = [700, 300];
+  formatedOlympics: ChartLine[] = [];
+  formatedParticipation!: FormatedParticipation[];
+  olympicId: number = 0;
+  totalParticipation: number = 0
+  totalMedals: number = 0
+  totalAthletes: number = 0
+  olympic: Olympic | undefined;
 
-  legend: boolean = false;
-  showLabels: boolean = true;
-  animations: boolean = true;
-  xAxis: boolean = true;
-  yAxis: boolean = true;
-  showYAxisLabel: boolean = true;
-  showXAxisLabel: boolean = true;
-  xAxisLabel: string = 'Dates';
-  yAxisLabel: string = 'Medals';
-  timeline: boolean = true;
-
-  colorScheme: Color = { 
-    domain: ['#647c8a',
-      '#3f51b5',
-      '#2196f3',
-      '#00b862',
-      '#afdf0a',
-      '#a7b61a',
-      '#f3e562',
-    ], 
-    group: ScaleType.Ordinal, 
-    selectable: true, 
-    name: 'Customer Usage', 
-};
-
-
-
-  public formatedOlympics: ChartLine[] = [];
-  public olympicsMedals!: OlympicsMedals[];
-  public olympicId: number = 0;
-  public olympicName!: string;
-  public totalParticipation!:number;
-  public totalMedals:number = 0
-  public totalAthletes: number = 0
-  public olympic!: Olympic;
-
- 
-  constructor(private olympicService: OlympicService, private route:ActivatedRoute){}
+  constructor(private olympicService: OlympicService, private route:ActivatedRoute, public chartGraphService: ChartGraphService){
+  }
   
   ngOnInit(): void {
     this.olympicId = +this.route.snapshot.params['id']
+
     this.olympicService.getOlympicsAsObservable().subscribe(
       olympics =>{
         this.olympic = olympics.find((olympic:Olympic) =>  {
            return  olympic.id == this.olympicId 
         })
-
-        //this.olympicName = olympic.country;
-        this.olympicsMedals = this.olympic.participations.map((participations:any) => {
-          this.totalMedals += participations.medalsCount
-          this.totalAthletes += participations.athleteCount
-          return {name:participations.year, value:participations.medalsCount}
-        })
-        this.totalParticipation = this.olympic.participations.length
-        this.formatedOlympics = [{name:this.olympic.country, series:this.olympicsMedals}]
+        if(this.olympic){
+          const participations: Participation[] = this.olympic.participations
+          this.formatedParticipation = this.retrieveFormatedParticipation(participations)
+          this.totalAthletes = this.retrieveTotalNumberOfAthletes(participations)
+          this.totalMedals = this.retrieveTotalNumberOfMedals(participations)
+          this.totalParticipation = participations.length
+          this.formatedOlympics = [{name:this.olympic.country, series:this.formatedParticipation}]
+        }
       }
     )
   }
-  
 
+
+  retrieveFormatedParticipation(participations:Participation[]): FormatedParticipation[]{
+    const formatedParticipation = participations.map((participations:Participation) => {
+      return {name:participations.year, value:participations.medalsCount};
+    }) 
+    return formatedParticipation;
+  }
+
+  retrieveTotalNumberOfAthletes(participations:Participation[]): number {
+    let totalNumberOfAthletes = 0;
+    participations.map((participations:Participation) => {
+      totalNumberOfAthletes += participations.athleteCount
+    }) 
+    return totalNumberOfAthletes;
+  }
+  
+  retrieveTotalNumberOfMedals(participations: Participation[]): number{
+    let totalNumberOfMedals = 0;
+    participations.map((participations:Participation) => {
+      totalNumberOfMedals += participations.medalsCount
+    }) 
+    return totalNumberOfMedals ;
+  }
+  
 }
 
